916b87b2d9941699c804d1b5afa08499bdde72e0
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-11-13 10:51:43 +0700

    [IMPROVE] password request

--- src/main/java/com/enigma/metawallet/model/request/UserRegisterRequest.java
+++ src/main/java/com/enigma/metawallet/model/request/UserRegisterRequest.java
@@ -19,7 +19,7 @@ public class UserRegisterRequest {
     private String email;
 
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d#$@!%&*?]{8,20}$",
-            message = "please must enter min 1 uppercase, min 1 lowercase, min 1 special character, min 1 number, min 8 characters and max 20 characters")
+            message = "please must enter min 1 uppercase, min 1 lowercase, min 1 number, min 8 characters and max 20 characters")
     private String password;
 
     @NotBlank
